d469c54d341bb99e92440b7189b21ffc717a2cf5
sander.bilo <sander@proteon.nl>
2012-02-03 10:08:00 +0000

    SB: added uid as openId

--- plugins/hooks/conext-login-hook/docroot/WEB-INF/src/nl/proteon/liferay/surfnet/security/auth/ConextAutoLogin.java
+++ plugins/hooks/conext-login-hook/docroot/WEB-INF/src/nl/proteon/liferay/surfnet/security/auth/ConextAutoLogin.java
@@ -9,9 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
 import com.liferay.portal.security.auth.AutoLogin;
 import com.liferay.portal.security.auth.AutoLoginException;
+import com.liferay.portal.security.ldap.PortalLDAPImporterUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortalUtil;
+import com.liferay.util.portlet.PortletProps;
 
 public class ConextAutoLogin implements AutoLogin {
 
@@ -23,23 +29,40 @@ public class ConextAutoLogin implements AutoLogin {
 		
 		try {
 			long companyId = PortalUtil.getCompanyId(request);
+			
+			String uniqueId = StringPool.BLANK;
+			String emailAddress = StringPool.BLANK;
+			String screenName = StringPool.BLANK;
+			
+			User user = null;
 
 			List<String> headers = Collections.list((Enumeration<String>)request.getHeaderNames());
 			for(String headerName : headers) {
 				_log.info(headerName + ": " + request.getHeader(headerName));
 			}
 			
+			if(Validator.isNotNull(request.getHeader(PortletProps.get("saml2.header.mapping.email")))) {
+				emailAddress = request.getHeader(PortletProps.get("saml2.header.mapping.email"));
+			}
+			if(Validator.isNotNull(request.getHeader(PortletProps.get("saml2.header.mapping.screenname")))) {
+				screenName = request.getHeader(PortletProps.get("saml2.header.mapping.screenname"));
+			}
+			if(Validator.isNotNull(request.getHeader(PortletProps.get("saml2.header.mapping.id")))) {
+				uniqueId = request.getHeader(PortletProps.get("saml2.header.mapping.id"));
+			}
 			
+			user = UserLocalServiceUtil.getUserByOpenId(companyId, uniqueId);
+
+			user = PortalLDAPImporterUtil.importLDAPUser(
+					companyId, emailAddress, screenName);
 			
-			//User user = PortalLDAPImporterUtil.importLDAPUser(
-			//		companyId, StringPool.BLANK, login);
-			
+			user.setOpenId(uniqueId);
 			
 			credentials = new String[3];
 
-			//credentials[0] = String.valueOf(user.getUserId());
-			//credentials[1] = user.getPassword();
-			//credentials[2] = Boolean.TRUE.toString();
+			credentials[0] = String.valueOf(user.getUserId());
+			credentials[1] = user.getPassword();
+			credentials[2] = Boolean.TRUE.toString();
 		}
 		catch (Exception e) {
 			_log.error(e, e);
